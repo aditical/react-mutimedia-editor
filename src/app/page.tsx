11d24b33202c3d5121c245a523a187e7901e6cc5
aditi <alishaacharya77@gmail.com>
2024-10-02 14:13:22 +0545

    set a layout for the editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import MediaPlayer from "@/sections/MediaPlayer";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[3fr_2fr] h-screen">
-      <div className="grid grid-cols-2 border-b border-gray-400">
-        <div className="border-r border-gray-400 p-4">Top Div 1</div>
-        <div className="p-4">Top Div 2</div>
+    <div>
+      <Header />
+      <div className="grid grid-cols-[5%_1fr] h-screen">
+        <div className="p-4 border border-white-400"><Sidebar /></div>
+        <div className="grid grid-cols-[25%_2fr] h-screen">
+          <div className="p-4 border border-gray-400">Side Div 2</div>
+          <div className="grid grid-rows-[3fr_2fr]">
+            <div className="p-4 border border-gray-400">
+              <MediaPlayer />
+            </div>
+            <div className="p-4 border border-gray-400">Bottom Div 2</div>
+          </div></div>
       </div>
-
-      <div className="p-4">Bottom Div</div>
     </div>
   );
 }
---
+++ src/sections/MediaPlayer.tsx
@@ -0,0 +1,7 @@
+export default function MediaPlayer() {
+    return (
+        <div className="">
+            Top Div
+        </div>
+    );
+}
